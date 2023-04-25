721ca44e20b0a5772d64c72a12889bb34b89b131
Sebastian Ballen C <sebastian.ballenc@gmail.com>
2023-04-24 22:57:46 -0500

    feat: more rules were added with their corresponding example

--- starter/script.js
+++ starter/script.js
@@ -19,4 +19,14 @@ let 3years = 3; //Cannot start with numbers
 let juan&adriana = 'JA'; // Can only contain letters, numbers, underscore or $ sign
 // Cannot use reserved keywords of JS
 let new = 27;
-let function = 'Walk' 
+let function = 'Walk';
+let Person = 'John' // The uppercase naming is used for an specific use like OOP
+let PI = 3.14 // A name write in all upercase is used to create constants, which are variables that it's value would not change
+
+// A good practice in naming to have cleaner & readable code is to be descriptive
+let myFirstJob = 'Librarian';
+let myCurrentJob = 'Software Engineer'
+
+// Not a very good practice
+let job1 = 'Librarian';
+let job2 = 'Software Engineer';
