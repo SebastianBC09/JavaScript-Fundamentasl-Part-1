d9eaa7070a5463a3b2e86aaf6c69660187d48ceb
Sebastian Ballen C <sebastian.ballenc@gmail.com>
2023-04-24 23:03:34 -0500

    feat: solution of first assignment completed

--- starter/assignments.js
+++ starter/assignments.js
@@ -1,1 +1,8 @@
 // Fundamentals - Part 1: Values & Variables
+let myCountry = 'Colombia';
+let myContinent = 'South America';
+let contryPopulation = 52000000
+
+console.log(myCountry)
+console.log(myContinent)
+console.log(contryPopulation)
