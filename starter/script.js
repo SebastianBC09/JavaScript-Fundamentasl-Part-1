32178d95727081d25cf4a445b90e1cf1e3994bb4
Sebastian Ballen C <sebastian.ballenc@gmail.com>
2023-04-24 22:47:18 -0500

    feat: learn the hard rules of naming in JS & what cannot be done

--- starter/script.js
+++ starter/script.js
@@ -5,10 +5,18 @@ console.log("Juan");
 console.log(23);
 
 let firstName = 'Juan';
-let first = 'Juan';
+let first = 'Adriana';
 let firstNamePerson = 'Camel case naming'
 let first_name_person = 'Underscore naming'
 
 console.log(firstName)
 console.log(firstName)
 console.log(firstName)
+
+// Hard Rules in Naming
+
+let 3years = 3; //Cannot start with numbers
+let juan&adriana = 'JA'; // Can only contain letters, numbers, underscore or $ sign
+// Cannot use reserved keywords of JS
+let new = 27;
+let function = 'Walk' 
