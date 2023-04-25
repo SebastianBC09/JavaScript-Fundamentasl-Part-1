d6e23cdfd70fb51253c9830906a8c287686535e5
Sebastian Ballen C <sebastian.ballenc@gmail.com>
2023-04-24 22:29:14 -0500

    feat: learning the different naming ways for variables, for JavaScript the standard is Camel case

--- starter/script.js
+++ starter/script.js
@@ -1,8 +1,14 @@
 let js = "Amazing";
 console.log(40 + 8 + 23 - 10);
 
-console.log("Juan")
-console.log(23)
+console.log("Juan");
+console.log(23);
 
-let firstName = 'Juan'
+let firstName = 'Juan';
+let first = 'Juan';
+let firstNamePerson = 'Camel case naming'
+let first_name_person = 'Underscore naming'
+
+console.log(firstName)
+console.log(firstName)
 console.log(firstName)
